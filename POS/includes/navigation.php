33d6ebd7329e4ef61072519b92912ec4dbb04dd0
Emmanuel Ugaban <ugabane0516@gmail.com>
2024-01-08 17:00:19 +0800

    format

--- POS/includes/navigation.php
+++ POS/includes/navigation.php
@@ -1,21 +1,29 @@
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/css/navigation.css">
     <title>navigation</title>
 </head>
+
 <body>
 
     <nav>
-        
-        
-                <ul class="standard-menu" onclick="window.location.href = '<?php if($db_acc_type=="administrator"){ echo '../../administrator/admin_view/index.php';}else{ echo '#';} ?>';">
-                    <li><img src="../assets/images/logos.png" alt="LOGO"></li>
-                    <li><h4 class="fw-bolder">RDPOS</h4></li>
-                
-                </ul>
-        
+
+
+        <ul class="standard-menu" onclick="window.location.href = '<?php if ($db_acc_type == "administrator") {
+                                                                        echo '../../administrator/admin_view/index.php';
+                                                                    } else {
+                                                                        echo '#';
+                                                                    } ?>';">
+            <li><img src="../assets/images/logos.png" alt="LOGO"></li>
+            <li>
+                <h4 class="fw-bolder">RDPOS</h4>
+            </li>
+
+        </ul>
+
 
         <ul class="right-menu">
             <li><a class="btn a" href="../pages/pos">Pos</a></li>
@@ -23,14 +31,14 @@
             <li><a class="btn a" href="../pages/inventory">Inventory</a></li>
             <li><a class="btn a" href="../pages/listpurchase">Manage stocks</a></li>
             <li class=" dropdown">
-            <a class="btn a dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Settings
-            </a>
-            <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Profile</a></li>
-                <li><a class="dropdown-item" href="../pages/maintenance">Maintenance</a></li>
-                <li><a class="dropdown-item" href="../functions/logout.php">Logout</a></li>
-            </ul>
+                <a class="btn a dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Settings
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Profile</a></li>
+                    <li><a class="dropdown-item" href="../pages/maintenance">Maintenance</a></li>
+                    <li><a class="dropdown-item" href="../functions/logout.php">Logout</a></li>
+                </ul>
             </li>
 
         </ul>
@@ -40,4 +48,5 @@
     </nav>
 
 </body>
+
 </html>
