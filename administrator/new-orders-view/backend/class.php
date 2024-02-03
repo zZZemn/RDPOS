c8249b3164ddd68c37f63a4e79ec9e38f9a78c1d
Emmanuel Ugaban <ugabane0516@gmail.com>
2024-02-03 21:26:58 +0800

    deduct in the inventory when the order is ready to deliver

--- administrator/new-orders-view/backend/class.php
+++ administrator/new-orders-view/backend/class.php
@@ -64,6 +64,50 @@ class global_class extends db_connect
                 return 400;
             }
 
+            if ($newStatus == 'Ready For Delivery') {
+                // Deduct to the inventory
+                $getItems = $this->conn->prepare("SELECT * FROM `new_tbl_order_items` WHERE `order_id` = '$orderId'");
+                if ($getItems->execute()) {
+                    $items = $getItems->get_result();
+                    while ($item = $items->fetch_assoc()) {
+                        $productId = $item['product_id'];
+                        $qty = $item['qty'];
+
+                        $inventorySql = $this->conn->prepare("SELECT * FROM `stocks` WHERE `s_prod_id` = '$productId' AND `s_amount` > 0 ORDER BY `s_expiration` ASC, `s_amount` DESC");
+                        if ($inventorySql->execute()) {
+                            $inv = $inventorySql->get_result();
+
+                            $detailQty = $qty;
+
+                            while ($inventoryRow = $inv->fetch_assoc()) {
+                                $availableQty = $inventoryRow['s_amount'];
+                                $subtractedQty = min($detailQty, $availableQty);
+
+                                $stockId = $inventoryRow['s_id'];
+                                $updateInvQty = $this->conn->prepare("UPDATE `stocks` SET `s_amount` = `s_amount` - $subtractedQty WHERE `s_id` = '$stockId'");
+
+                                if ($updateInvQty->execute()) {
+                                    if ($subtractedQty > 0) {
+                                        // Insert Sales Pag need
+                                        $detailQty -= $subtractedQty;
+                                    }
+                                } else {
+                                    return 'Update Stock SQL Error!';
+                                }
+
+                                if ($detailQty <= 0) {
+                                    break;
+                                }
+                            }
+                        } else {
+                            return 'Inventory SQL Error!';
+                        }
+                    }
+                } else {
+                    return 'Get Items SQL Error';
+                }
+            }
+
             $query = $this->conn->prepare("UPDATE `new_tbl_orders` SET `status`='$newStatus' WHERE `order_id` = '$orderId'");
             if ($query->execute()) {
                 return 200;
