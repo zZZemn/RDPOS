7696bba10644116c5dac60271baa6da06c59bf6c
Emmanuel Ugaban <ugabane0516@gmail.com>
2024-02-08 21:10:24 +0800

    view order

--- rider/backend/class.php
+++ rider/backend/class.php
@@ -18,6 +18,15 @@ class global_class extends db_connect
         }
     }
 
+    public function checkId($tableName, $columnName, $id)
+    {
+        $query = $this->conn->prepare("SELECT * FROM `$tableName` WHERE `$columnName` = '$id'");
+        if ($query->execute()) {
+            $result = $query->get_result();
+            return $result;
+        }
+    }
+
     public function getOrdersSetToRider($riderId, $status)
     {
         $query = $this->conn->prepare("SELECT * FROM `new_tbl_orders` WHERE `rider_id` = '$riderId' AND `status` = '$status'");
@@ -30,6 +39,15 @@ class global_class extends db_connect
     public function getOrderUsingOrderId($riderId, $orderId)
     {
         $query = $this->conn->prepare("SELECT * FROM `new_tbl_orders` WHERE `rider_id` = '$riderId' AND `order_id` = '$orderId'");
+        if ($query->execute()) {
+            $result = $query->get_result();
+            return $result;
+        }
+    }
+
+    public function getUserOrderItems($orderId)
+    {
+        $query = $this->conn->prepare("SELECT i.qty, p.* FROM `new_tbl_order_items` AS i JOIN `product` AS p ON i.product_id = p.prod_id WHERE i.order_id = '$orderId'");
         if ($query->execute()) {
             $result = $query->get_result();
             return $result;
